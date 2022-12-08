cb593525f40b831e45578862f876be4c7e20d566
eralash911 <unreal725@gmail.com>
2022-12-08 14:40:12 +0300

    Initial commit

---
+++ src/main/java/com/example/h2testdelete/H2testDeleteApplication.java
@@ -0,0 +1,13 @@
+package com.example.h2testdelete;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class H2testDeleteApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(H2testDeleteApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/h2testdelete/H2testDeleteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.h2testdelete;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class H2testDeleteApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
