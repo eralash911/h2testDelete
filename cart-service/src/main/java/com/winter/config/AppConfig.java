436bd60df891ce7b4d3f44f07538694127bf95cc
eralash911 <unreal725@gmail.com>
2022-12-16 15:24:40 +0300

    properties configurations

--- cart-service/src/main/java/com/winter/config/AppConfig.java
+++ cart-service/src/main/java/com/winter/config/AppConfig.java
@@ -1,13 +1,48 @@
 package com.winter.config;
 
+import com.winter.properties.ProductServiceIntegrationsProperties;
+import io.netty.channel.ChannelOption;
+import io.netty.handler.timeout.ReadTimeoutHandler;
+import io.netty.handler.timeout.WriteTimeoutHandler;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.client.reactive.ReactorClientHttpConnector;
 import org.springframework.web.client.RestTemplate;
+import org.springframework.web.reactive.function.client.WebClient;
+import reactor.netty.http.client.HttpClient;
+import reactor.netty.tcp.TcpClient;
+
+import java.util.concurrent.TimeUnit;
 
 @Configuration
+@EnableConfigurationProperties(
+        ProductServiceIntegrationsProperties.class
+)
+@RequiredArgsConstructor
 public class AppConfig {
+    private final ProductServiceIntegrationsProperties properties;
+//    @Bean
+//    public RestTemplate restTemplate(){
+//        return new RestTemplate();
+//    }
+
     @Bean
-    public RestTemplate restTemplate(){
-        return new RestTemplate();
+    public WebClient productServiceWebClient() {
+        TcpClient tcpClient = TcpClient
+                .create()
+                .option(ChannelOption.CONNECT_TIMEOUT_MILLIS, properties.getConnectTimeout())
+                .doOnConnected(connection -> {
+                    connection.addHandlerLast(new ReadTimeoutHandler(properties.getReadTimeout(), TimeUnit.MILLISECONDS));
+                    connection.addHandlerLast(new WriteTimeoutHandler(properties.getWriteTimeout(), TimeUnit.MILLISECONDS));
+                });
+
+        return WebClient
+                .builder()
+                .baseUrl(properties.getUrl())
+                .clientConnector(new ReactorClientHttpConnector(HttpClient.from(tcpClient)))
+                .build();
     }
 }
--- cart-service/src/main/java/com/winter/integration/ProductServiceIntegration.java
+++ cart-service/src/main/java/com/winter/integration/ProductServiceIntegration.java
@@ -1,20 +1,39 @@
 package com.winter.integration;
 
 import com.example.api.ProductDto;
+import com.example.api.ResourceNotFoundException;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
 import org.springframework.stereotype.Component;
+import org.springframework.web.client.ResourceAccessException;
 import org.springframework.web.client.RestTemplate;
+import org.springframework.web.reactive.function.client.WebClient;
+import reactor.core.publisher.Mono;
 
 import java.util.Optional;
 
 @Component
 @RequiredArgsConstructor
 public class ProductServiceIntegration {
+    private final WebClient productServiceWebClient;
 
-    private final RestTemplate restTemplate;
-
-    public Optional<ProductDto> getProductById(Long id){
-        return Optional.ofNullable( restTemplate.getForObject("http://localhost:/winter/api/products/" + id, ProductDto.class));
-
+    public ProductDto findById(Long id) {
+        return productServiceWebClient.get()
+                .uri("/api/v1/products/" + id)
+                .retrieve()
+                .onStatus(httpStatus -> httpStatus.value() == HttpStatus.NOT_FOUND.value(),
+                        clientResponse -> Mono.error(new ResourceNotFoundException("not found")))
+                .bodyToMono(ProductDto.class)
+                .block();
     }
+//
+//    public void clear(String username) {
+//        cartServiceWebClient.get()
+//                .uri("/api/v1/cart/0/clear")
+//                .header("username", username)
+//                .retrieve()
+//                .toBodilessEntity()
+//                .block();
+//    }
 }
