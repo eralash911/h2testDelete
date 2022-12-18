326af994df35121cea7a744f5b598c1a595179fa
eralash911 <unreal725@gmail.com>
2022-12-18 20:56:10 +0300

    gateway added

--- core_sec_service/src/main/java/com/winter/core/controller/OrderController.java
+++ core_sec_service/src/main/java/com/winter/core/controller/OrderController.java
@@ -6,16 +6,14 @@ import com.winter.core.service.OrderService;
 import com.winter.core.service.UserService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseStatus;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.security.Principal;
 
 @RestController
 @RequestMapping("api/v1/order")
 @RequiredArgsConstructor
+@CrossOrigin("*")
 public class OrderController {
     private final OrderService orderService;
     private final UserService userService;
